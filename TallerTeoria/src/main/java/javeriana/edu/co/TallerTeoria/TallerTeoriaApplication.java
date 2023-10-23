079c033649ff02393be96164ee16098d4081943f
Lopezcg <124104040+Lopezcg@users.noreply.github.com>
2023-10-23 13:59:58 -0500

    Add files via upload

---
+++ TallerTeoria/src/main/java/javeriana/edu/co/TallerTeoria/TallerTeoriaApplication.java
@@ -0,0 +1,13 @@
+package javeriana.edu.co.TallerTeoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerTeoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerTeoriaApplication.class, args);
+	}
+
+}
---
+++ TallerTeoria/src/test/java/javeriana/edu/co/TallerTeoria/TallerTeoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package javeriana.edu.co.TallerTeoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerTeoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
